94494568608129b511632476866d59bc188ec904
8ceg6 <8ceg6@comcast.net>
2020-05-31 10:58:26 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'sessions/create'
+  root 'welcome#home'
   # Add your routes here
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+ 
+  get '/auth/github/callback' => 'sessions#create'
 end
